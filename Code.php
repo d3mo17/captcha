df1191eb7419683852ca67295299c4f65a0002b7
Daniel Moritz <d.moritz@gmx.org>
2022-02-25 17:55:20 +0100

    First implementation with unit tests

---
+++ Code.php
@@ -0,0 +1,262 @@
+<?php
+
+namespace DMo\Captcha;
+
+/**
+ * Generates an array with seven keys for seven input fields in a form.
+ */
+class Code {
+    /**
+     * Defines the length of output values in code array.
+     * Reasonable range is 1 to 13
+     * 
+     * @var int
+     */
+    protected $CHUNK_LENGTH = 13;
+    /**
+     * Code is valid for this duration and expires after this time
+     * 
+     * @var int
+     */
+    protected $validForMinutes = 12;
+    /**
+     * Code is invalid before these seconds have past
+     * 
+     * @var int
+     */
+    protected $validNotTillSeconds = 3;
+
+    /**
+     * @var string
+     */
+    private $secretPepper;
+    /**
+     * @var int
+     */
+    private $timestamp;
+
+    /**
+     * @var \Error|null
+     */
+    private $error;
+
+
+    /**
+     * Array of randomly ordered decimal numbers which have at least two same bits in
+     * binary signature (means at least two zeros and two ones).
+     * This is to determine which keys in code array have to be set an which last empty.
+     * The code array will have seven keys, so this is the reason why only numbers with
+     * at most seven bits are used.
+     * 
+     * Up to you to rearrange this list in a subclass!
+     *
+     * @var array
+     */
+    protected $numberSequence = [
+        113, 115, 116, 118, 124, 121, 114, 117, 127, 122, 120,   6,  14,  12,   9,   5,
+         81,  83,  93,  84,  91,  86,  94,  92,  89,  82,  85,  90,  87,  88,  96,   3,
+        104, 105, 107, 108, 102, 110, 106, 109,  98, 101, 100,  99, 112,  97, 103,  15,
+         24,  25,  27,  28,  22,  30,  26,  29,  18,  21,  20,  19,  31,  17,  23,  10,
+         77,  68,  66,  69,  74,  71,  65,  70,  79,  80,  78,  76,  72,  73,  75,  67,
+         43,  44,  38,  46,  47,  48,  40,  34,  42,  39,  37,  36,  33,  35,  45,  41,
+         59,  60,  54,  62,  56,  49,  51,  61,  57,  50,  58,  55,  53,  52,   7,  11,
+    ];
+
+
+    /**
+     * @param string $secretPepper
+     * @param int $time
+     */
+    public function __construct(
+        $secretPepper = '<SecretString>',
+        $time = null
+    ) {
+        $this->secretPepper = $secretPepper;
+        $this->timestamp = $time ?: time();
+    }
+
+    /**
+     * Returns the needed code sequence
+     * 
+     * @return array
+     */
+    public function get() : array {
+        $code = array_pad([], 7, '');
+        $indices = $this->getIndicesToBeFilled();
+        $hashChunks = $this->getHashChunks();
+
+        $index = reset($indices);
+        $code[$index] = $this->currentSecondInPeriod() . ':' . $this->getMinuteOfDay();
+        $chunk = reset($hashChunks);
+        while ($index = next($indices)) {
+            $code[$index] = $chunk;
+            $chunk = next($hashChunks);
+        }
+
+        return $code;
+    }
+
+    /**
+     * @param array $codeArray
+     * 
+     * @return bool
+     */
+    public function validate($codeArray) : bool {
+        $removedEmpty = array_diff($codeArray, ['']);
+        list($secondToCompare, $minuteToCompare) = explode(':', reset($removedEmpty));
+
+        $minuteOfDay = $this->getMinuteOfDay() < $minuteToCompare
+            ? (24*60) + $this->getMinuteOfDay()
+            : $this->getMinuteOfDay();
+        $duration = $minuteOfDay - $minuteToCompare;
+        if ($duration > $this->validForMinutes) {
+            $this->error = new \Error('Expired (lasts ' . $duration . ' min.)!', 1645535407);
+            return false;
+        }
+
+        $currentSecondInPeriod = $this->currentSecondInPeriod() < $secondToCompare
+            ? ($this->validForMinutes * 60) + $this->currentSecondInPeriod()
+            : $this->currentSecondInPeriod();
+        $duration = $currentSecondInPeriod - $secondToCompare;
+        if ($duration <= $this->validNotTillSeconds) {
+            $this->error = new \Error('Too fast input for human beings (' . $duration . ' sec.)!', 1645532843);
+            return false;
+        }
+
+        if (!$this->validateSignature($codeArray)) {
+            $this->error = new \Error('Signature validation of code array fails!', 1645535534);
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Returns theLast Error, if there is one (after validation fails)
+     * @return \Error|null
+     */
+    public function getLastError() {
+        return $this->error;
+    }
+
+
+    /**
+     * Get minute of current day from timestamp.
+     * 
+     * @return int
+     */
+    final public function getMinuteOfDay() : int {
+        return intval(date('H', $this->timestamp)) * 60 + intval(date('i', $this->timestamp)) + 1;
+    }
+
+    /**
+     * Has the purpose to obscure the order of number sequence depending on minute of day.
+     * Up to you to override it in a subclass for more individuality. But consider this has to be consistent
+     * in each moment
+     * 
+     * @return int
+     */
+    protected function getShiftingNumber() : int {
+        return intval(date('Y', $this->timestamp))
+                - intval(date('d', $this->timestamp))
+                - intval(date('m', $this->timestamp)) * 15;
+    }
+
+    /**
+     * @return int
+     */
+    private function getIndexOfPeriod() : int {
+        return (ceil($this->getMinuteOfDay() / $this->validForMinutes) + $this->getShiftingNumber()) % 112;
+    }
+
+    /**
+     * Returns which indices of code array have to be set.
+     * 
+     * @return array
+     */
+    private function getIndicesToBeFilled() : array {
+        $decNum = $this->numberSequence[$this->getIndexOfPeriod()];
+        $binCode = str_pad(decbin($decNum), 7, '0', STR_PAD_LEFT);
+        return array_keys(array_diff(str_split($binCode), [0]));
+    }
+
+    /**
+     * Returns an array containing three hash strings of configured length
+     * 
+     * @return array
+     */
+    private function getHashChunks() : array {
+        $chunks = str_split(sha1($this->secretPepper.$this->getIndexOfPeriod()), $this->CHUNK_LENGTH);
+        array_pop($chunks);
+        return $chunks;
+    }
+
+    /**
+     * Returns the significant second in current period
+     * 
+     * @return integer
+     */
+    final protected function currentSecondInPeriod() : int {
+        return ($this->timestamp % ($this->validForMinutes * 60)) + 1;
+    }
+
+    /**
+     * Compares two values under the aspect they can be compared
+     * 
+     * @param string $expectation
+     * @param string $givenValue
+     * 
+     * @return bool
+     */
+    private function compareValues(string $expectation, string $givenValue) : bool {
+        if (empty($expectation) && !empty($givenValue)) {
+            return false;
+        }
+
+        if (strlen($expectation) === $this->CHUNK_LENGTH && $expectation !== $givenValue) {
+            return false;
+        }
+
+        // ignore expectations containing time values (the first two values of code array)
+
+        return true;
+    }
+
+    /**
+     * Validates the passed array
+     * 
+     * @param array $codeArray
+     * 
+     * @return bool
+     */
+    private function validateSignature(array $codeArray) : bool {
+        $expectations = $this->get();
+
+        $valid = true;
+        foreach ($expectations as $key => $expected) {
+            $valid = $valid && $this->compareValues($expected, $codeArray[$key]);
+        }
+
+        if ($valid) {
+            return true;
+        }
+
+        // As a function of tolarance, we have to check values from the period before
+        $lastCaptcha = new static();
+        $lastCaptcha->secretPepper = $this->secretPepper;
+        $lastCaptcha->timestamp = $this->timestamp - ($this->validForMinutes * 60);
+
+        $expectations = $lastCaptcha->get();
+
+        $valid = true;
+        foreach ($expectations as $key => $expected) {
+            $valid = $valid && $this->compareValues($expected, $codeArray[$key]);
+        }
+
+        if ($valid) {
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ tests/CodeTest.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace DMo\Captcha\Tests;
+
+use DMo\Captcha\Code;
+use PHPUnit\Framework\TestCase;
+
+class CodeTest extends TestCase
+{
+    private $time;
+    private $time2;
+
+    private $cap;
+    private $cap1;
+    private $cap2;
+    private $cap3;
+    private $cap4;
+    private $cap5;
+
+    public function __construct() {
+        parent::__construct();
+
+        // no hashes in code array
+        $this->time  = mktime(14, 34, 15, 3, 17, 78);
+        // three hashes in code array
+        $this->time2 = mktime(14, 34, 15, 3, 18, 78);
+
+        $this->cap  = new Code('secret',    $this->time);
+        $this->cap1 = new Code('secret',    $this->time2);
+        $this->cap2 = new Code('secret',    $this->time2);
+        $this->cap3 = new Code('secret123', $this->time2);
+        $this->cap4 = new Code('secret',    $this->time2 + 1);
+        $this->cap5 = new Code('secret',    $this->time2 + 6*60);
+        $this->cap6 = new Code('secret',    $this->time2 + 6);
+        $this->cap7 = new Code('secret',    $this->time2 + 13*60);
+        $this->cap8 = new Code('secret',    $this->time2 + 3);
+        $this->cap9 = new Code('secret123', $this->time2 + 5);
+    }
+
+    public function testFormalStructure() {
+        $this->assertIsArray($this->cap->get());
+        $this->assertCount(7, $this->cap->get());
+
+        foreach (range(1, 9) as $i) {
+            $field = "cap$i";
+            $this->assertIsArray($this->{$field}->get());
+            $this->assertCount(7, $this->{$field}->get());
+        }
+    }
+
+    public function testEquality() {
+        $this->assertEquals($this->cap1->get(), $this->cap2->get());
+
+        // not equal, cause of first (time containing) entry only
+        $this->assertNotEquals($this->cap1->get(), $this->cap4->get());
+        // equal in other entries
+        $diff = array_diff($this->cap1->get(), $this->cap4->get());
+        $this->assertCount(1, $diff);
+
+        // not equal at all, cause of different hash entries
+        $this->assertNotEquals($this->cap1->get(), $this->cap3->get());
+        $diff = array_diff($this->cap1->get(), $this->cap3->get());
+        $this->assertNotCount(1, $diff);
+    }
+
+    public function testValid() {
+        // valid, cause in same period
+        $this->assertTrue($this->cap6->validate($this->cap1->get()));
+        // with same hash signature
+        $diff = array_diff($this->cap6->get(), $this->cap1->get());
+        $this->assertCount(1, $diff);
+
+        // valid, cause in same period
+        $this->assertTrue($this->cap7->validate($this->cap5->get()));
+        // with same hash signature
+        $diff = array_diff($this->cap7->get(), $this->cap5->get());
+        $this->assertCount(1, $diff);
+
+        // valid, cause in same period
+        $this->assertTrue($this->cap6->validate($this->cap4->get()));
+        // with same hash signature
+        $diff = array_diff($this->cap6->get(), $this->cap4->get());
+        $this->assertCount(1, $diff);
+
+        // different (hash values), cause of different time periods
+        $diff = array_diff($this->cap5->get(), $this->cap4->get());
+        $this->assertNotCount(1, $diff);
+        // but valid, cause of absolute defined period to be valid
+        $this->assertTrue($this->cap5->validate($this->cap4->get()));
+    }
+
+    public function testInValid() {
+        // different secret strings
+        $this->assertFalse($this->cap9->validate($this->cap2->get()));
+        $this->assertEquals(1645535534, $this->cap9->getLastError()->getCode());
+
+        // to fast for human interaction
+        $this->assertFalse($this->cap4->validate($this->cap2->get()));
+        $this->assertEquals(1645532843, $this->cap4->getLastError()->getCode());
+        $this->assertFalse($this->cap6->validate($this->cap8->get()));
+        $this->assertEquals(1645532843, $this->cap6->getLastError()->getCode());
+
+        // last too long
+        $this->assertFalse($this->cap7->validate($this->cap2->get()));
+        $this->assertEquals(1645535407, $this->cap7->getLastError()->getCode());
+
+        // Fields not empty where they have to be empty (honey pot)
+        $code = $this->cap1->get();
+        $code[2] = 'I\'m a robot!';
+        $diff = array_diff($code, $this->cap6->get());
+        $this->assertCount(2, $diff);
+        $this->assertFalse($this->cap6->validate($code));
+        $this->assertEquals(1645535534, $this->cap6->getLastError()->getCode());
+    }
+}
